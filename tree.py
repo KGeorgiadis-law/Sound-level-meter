f1d58d4007fdd56348e690ad240fa785434e4cef
Konstantinos Georgiadis <kostas.georgiadis363@gmail.com>
2022-01-26 09:35:50 +0000

    Update tree.py
    Working version, tested on rpi3

--- tree.py
+++ tree.py
@@ -11,23 +11,31 @@ tree = LEDBoard(*range(2,28),pwm=True)
 
 tree[0].pulse(2, 2)
 
-duration = 10  # seconds
+duration = 60  # seconds
 
 def light_up_tree(indata, outdata, frames, time, status):
     volume_norm = int(np.linalg.norm(indata)*10)
-    print (volume_norm)
     
-    max_volume = 1
+    
+    max_volume = 100
+    
+    if volume_norm >= 100:
+        volume_norm = 100
+    
     volume_to_leds = int((volume_norm/max_volume)*25)
+
+    print (volume_norm,volume_to_leds)
     
     if volume_to_leds == 0:
         volume_to_leds = 1
     
     for i in range(1, volume_to_leds):
-        led[i].on()
+        tree[i].on()
+    sleep(0.005)
+       
     for i in range(volume_to_leds,25):
         led[i].off()
-    sleep(0.05)
+    sleep(0.001)
 
 with sd.Stream(callback=light_up_tree):
     sd.sleep(duration * 1000)
